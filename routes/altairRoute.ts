3c75896a6a83f5d4c4f70eaf08545c2453753063
Muhammad Luqmanul Hakim <iamhkmid@gmail.com>
2023-01-19 15:50:26 +0700

    git init

--- routes/altairRoute.ts
+++ routes/altairRoute.ts
@@ -3,18 +3,20 @@ import express from "express";
 
 const altairRoute = express.Router();
 
-altairRoute.use(
-  "/",
-  (req, res, next) => {
-    process.env.NODE_ENV === "development"
-      ? next()
-      : res.end("Development only");
-  },
+type TCheckEnv = (req: express.Request, res: express.Response, next: express.NextFunction) => void
+
+const checkEnv: TCheckEnv = (req, res, next) => {
+  process.env.NODE_ENV === "development"
+    ? next()
+    : res.end("Development only");
+}
+
+altairRoute.use("/", () => {
   altairExpress({
     endpointURL: "/graphql",
     subscriptionsEndpoint: `ws://localhost:3001/subscriptions`,
     initialQuery: ``,
   })
-);
+});
 
 export default altairRoute;
