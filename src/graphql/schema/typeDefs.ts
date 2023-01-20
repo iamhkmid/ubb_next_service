01a596f5b5c7c8d0061ba60b60a1c830e92f809b
Muhammad Luqmanul Hakim <iamhkmid@gmail.com>
2023-01-20 18:53:45 +0700

    add yarn lock

--- src/graphql/schema/typeDefs.ts
+++ src/graphql/schema/typeDefs.ts
@@ -3,6 +3,5 @@ import { loadFilesSync } from "@graphql-tools/load-files"
 import { mergeTypeDefs } from "@graphql-tools/merge"
 
 const typesArray = loadFilesSync(path.join(process.cwd(), 'src/graphql/schema/**/*.graphql'), { recursive: true })
-console.log({__dirname})
-console.log({typesArray})
+
 export default mergeTypeDefs(typesArray)
