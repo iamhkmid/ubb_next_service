8ee3ccedcf6a912f1f2f4dc7be47145aa7d89d0e
Muhammad Luqmanul Hakim <iamhkmid@gmail.com>
2023-01-23 15:22:01 +0700

    update rsa login

--- src/graphql/schema/auth/auth.resolvers.ts
+++ src/graphql/schema/auth/auth.resolvers.ts
@@ -2,16 +2,18 @@ import { GraphQLError } from "graphql";
 import { MutationResolvers } from "../../../types/graphql";
 import bcrypt from "bcrypt"
 import { createToken } from "./utils";
+import { decryptRSA } from "../../../libs/rsa";
 
 export const Mutation: MutationResolvers = {
   login: async (_, args, { db, res }) => {
     const { username, password } = args
+    const decryptedPass = decryptRSA(password)
     const findUser = await db.user.findUnique({ where: { username } })
     if (!findUser) throw new GraphQLError('Username or Password incorrect', { extensions: { code: 'BAD_REQUEST' } })
 
     if (!findUser.isActive) throw new GraphQLError('Inactive account', { extensions: { code: 'FORBIDDEN' } })
 
-    const checkPw = await bcrypt.compare(password, findUser.password)
+    const checkPw = await bcrypt.compare(decryptedPass, findUser.password)
     if (!checkPw)
       throw new GraphQLError("Username or Password incorrect", { extensions: { code: 'FORBIDDEN' } })
 
