c42b114e72689aa27fdb10edbac56d84a2f6ffa8
Muhammad Luqmanul Hakim <iamhkmid@gmail.com>
2023-02-08 10:36:18 +0700

    add banner

--- dist/src/graphql/schema/scalar/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.resolvers = void 0;
-const Date_1 = require("./Date");
-exports.resolvers = {
-    Date: Date_1.dateScalar,
-};
